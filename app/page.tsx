0b3ba866cee2a5fc4efca20a0818ece552c63ef6
Nik Pietanze <nikpietanze@gmail.com>
2024-03-12 15:30:28 -0500

    initial designs

--- app/page.tsx
+++ app/page.tsx
@@ -1,112 +1,146 @@
-import Image from "next/image";
-
 export default function Home() {
-	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-				<p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-					Get started by editing&nbsp;
-					<code className="font-mono font-bold">app/page.tsx</code>
-				</p>
-				<div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-					<a
-						className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-						href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-						target="_blank"
-						rel="noopener noreferrer">
-						By{" "}
-						<Image
-							src="/vercel.svg"
-							alt="Vercel Logo"
-							className="dark:invert"
-							width={100}
-							height={24}
-							priority
-						/>
-					</a>
-				</div>
-			</div>
+    // TODO: Navigation scrolling
 
-			<div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-				<Image
-					className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-					src="/next.svg"
-					alt="Next.js Logo"
-					width={180}
-					height={37}
-					priority
-				/>
-			</div>
+    return (
+        <main className="bg-gradient-to-r from-zinc-900 to-slate-800 text-zinc-100 min-h-screen p-16 flex justify-center">
+            <div className="grid grid-cols-1 gap-14 items-start md:grid-cols-[400px_minmax(400px,_550px)]">
+                <div className="left-col flex flex-col md:sticky md:top-24">
+                    <div className="info flex flex-col gap-4 pb-24">
+                        <h1 className="text-5xl font-bold">Nik Pietanze</h1>
+                        <h2 className="text-xl font-bold">Senior Software Engineer</h2>
+                        <p className="text-zinc-300">I build scalable and performant data-focused
+                            <br /> web apps, utilizing multiple languages.</p>
+                    </div>
 
-			<div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-				<a
-					href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-					className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-					target="_blank"
-					rel="noopener noreferrer">
-					<h2 className={`mb-3 text-2xl font-semibold`}>
-						Docs{" "}
-						<span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-							-&gt;
-						</span>
-					</h2>
-					<p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-						Find in-depth information about Next.js features and
-						API.
-					</p>
-				</a>
+                    <div className="nav flex flex-col gap-4 text-sm font-bold text-emerald-700">
+                        <a href="#about" className="active">ABOUT</a>
+                        <a href="#experience">EXPERIENCE</a>
+                        <a href="#projects">PROJECTS</a>
+                    </div>
+                </div>
 
-				<a
-					href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-					className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-					target="_blank"
-					rel="noopener noreferrer">
-					<h2 className={`mb-3 text-2xl font-semibold`}>
-						Learn{" "}
-						<span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-							-&gt;
-						</span>
-					</h2>
-					<p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-						Learn about Next.js in an interactive course
-						with&nbsp;quizzes!
-					</p>
-				</a>
+                <div className="right-col flex flex-col gap-32">
+                    <div id="about" className="flex flex-col gap-4 text-zinc-300">
+                        <h2 className="text-sm font-bold text-emerald-600">ABOUT</h2>
 
-				<a
-					href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-					className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-					target="_blank"
-					rel="noopener noreferrer">
-					<h2 className={`mb-3 text-2xl font-semibold`}>
-						Templates{" "}
-						<span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-							-&gt;
-						</span>
-					</h2>
-					<p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-						Explore starter templates for Next.js.
-					</p>
-				</a>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tortor posuere ac ut consequat semper. Metus dictum at tempor commodo ullamcorper a lacus vestibulum sed. Sapien et ligula ullamcorper malesuada proin libero nunc consequat interdum. Libero justo laoreet sit amet cursus. Elit pellentesque habitant morbi tristique senectus. Amet risus nullam eget felis eget nunc lobortis mattis aliquam. Sit amet purus gravida quis. Mi in nulla posuere sollicitudin aliquam ultrices sagittis orci. Facilisi nullam vehicula ipsum a arcu.
+                        </p>
 
-				<a
-					href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-					className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-					target="_blank"
-					rel="noopener noreferrer">
-					<h2 className={`mb-3 text-2xl font-semibold`}>
-						Deploy{" "}
-						<span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-							-&gt;
-						</span>
-					</h2>
-					<p
-						className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-						Instantly deploy your Next.js site to a shareable URL
-						with Vercel.
-					</p>
-				</a>
-			</div>
-		</main>
-	);
+                        <p>
+                            Et leo duis ut diam. In hac habitasse platea dictumst vestibulum rhoncus est pellentesque. At auctor urna nunc id cursus metus aliquam. Ut sem viverra aliquet eget. Lorem sed risus ultricies tristique nulla aliquet enim tortor at. Cursus euismod quis viverra nibh. Ipsum faucibus vitae aliquet nec ullamcorper sit amet risus nullam. Nec feugiat nisl pretium fusce id velit ut tortor. Orci phasellus egestas tellus rutrum tellus pellentesque eu. Rutrum quisque non tellus orci ac auctor augue mauris augue. Eget mauris pharetra et ultrices neque. Aliquet sagittis id consectetur purus ut. Mi proin sed libero enim sed faucibus turpis.
+                        </p>
+
+                        <p>
+                            Dolor magna eget est lorem ipsum dolor sit amet. Pharetra magna ac placerat vestibulum. Nulla facilisi nullam vehicula ipsum a. Scelerisque varius morbi enim nunc. Nunc eget lorem dolor sed viverra ipsum nunc. Molestie nunc non blandit massa enim nec dui nunc. Id eu nisl nunc mi ipsum faucibus vitae aliquet. Enim blandit volutpat maecenas volutpat blandit. Ac tortor vitae purus faucibus ornare suspendisse sed nisi lacus. Suspendisse ultrices gravida dictum fusce ut. Risus sed vulputate odio ut enim. Tortor at risus viverra adipiscing at in tellus integer. At tellus at urna condimentum mattis pellentesque. Mauris augue neque gravida in fermentum. Facilisis leo vel fringilla est ullamcorper. Tristique senectus et netus et malesuada fames.
+                        </p>
+                    </div>
+
+                    <div id="experience" className="flex flex-col gap-8 -mx-4">
+                        <h2 className="text-sm font-bold text-emerald-600 -mb-6 px-4">EXPERIENCE</h2>
+
+                        <a href="#" className="grid grid-cols-[125px_auto] hover:bg-emerald-900/50 transition rounded-lg p-4">
+                            <span className="timeframe text-xs font-bold text-zinc-400 mt-1">2024 — PRESENT</span>
+                            <div className="details flex flex-col gap-2">
+                                <span className="pos-title text-lg font-bold">Senior Software Engineer</span>
+                                <p className="pos-description text-base text-zinc-300">
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae ultricies leo integer malesuada nunc vel risus commodo. Ac tincidunt vitae semper quis lectus nulla at volutpat diam.
+                                </p>
+
+                                <div className="tags flex flex-wrap items-center gap-4">
+                                    <span>JavaScript</span>
+                                    <span>TypeScript</span>
+                                    <span>React</span>
+                                </div>
+                            </div>
+                        </a>
+
+                        <a href="#" className="grid grid-cols-[125px_auto] hover:bg-emerald-900/50 transition rounded-lg p-4">
+                            <span className="timeframe text-xs font-bold text-zinc-400 mt-1">2024 — PRESENT</span>
+                            <div className="details flex flex-col gap-2">
+                                <span className="pos-title text-lg font-bold">Senior Software Engineer</span>
+                                <p className="pos-description text-base text-zinc-300">
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae ultricies leo integer malesuada nunc vel risus commodo. Ac tincidunt vitae semper quis lectus nulla at volutpat diam.
+                                </p>
+
+                                <div className="tags flex flex-wrap items-center gap-4">
+                                    <span>JavaScript</span>
+                                    <span>TypeScript</span>
+                                    <span>React</span>
+                                </div>
+                            </div>
+                        </a>
+
+                        <a href="#" className="grid grid-cols-[125px_auto] hover:bg-emerald-900/50 transition rounded-lg p-4">
+                            <span className="timeframe text-xs font-bold text-zinc-400 mt-1">2024 — PRESENT</span>
+                            <div className="details flex flex-col gap-2">
+                                <span className="pos-title text-lg font-bold">Senior Software Engineer</span>
+                                <p className="pos-description text-base text-zinc-300">
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae ultricies leo integer malesuada nunc vel risus commodo. Ac tincidunt vitae semper quis lectus nulla at volutpat diam.
+                                </p>
+
+                                <div className="tags flex flex-wrap items-center gap-4">
+                                    <span>JavaScript</span>
+                                    <span>TypeScript</span>
+                                    <span>React</span>
+                                </div>
+                            </div>
+                        </a>
+
+                        <a href="#" className="grid grid-cols-[125px_auto] hover:bg-emerald-900/50 transition rounded-lg p-4">
+                            <span className="timeframe text-xs font-bold text-zinc-400 mt-1">2024 — PRESENT</span>
+                            <div className="details flex flex-col gap-2">
+                                <span className="pos-title text-lg font-bold">Senior Software Engineer</span>
+                                <p className="pos-description text-base text-zinc-300">
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae ultricies leo integer malesuada nunc vel risus commodo. Ac tincidunt vitae semper quis lectus nulla at volutpat diam.
+                                </p>
+
+                                <div className="tags flex flex-wrap items-center gap-4">
+                                    <span>JavaScript</span>
+                                    <span>TypeScript</span>
+                                    <span>React</span>
+                                </div>
+                            </div>
+                        </a>
+
+                        <a href="#" className="grid grid-cols-[125px_auto] hover:bg-emerald-900/50 transition rounded-lg p-4">
+                            <span className="timeframe text-xs font-bold text-zinc-400 mt-1">2024 — PRESENT</span>
+                            <div className="details flex flex-col gap-2">
+                                <span className="pos-title text-lg font-bold">Senior Software Engineer</span>
+                                <p className="pos-description text-base text-zinc-300">
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae ultricies leo integer malesuada nunc vel risus commodo. Ac tincidunt vitae semper quis lectus nulla at volutpat diam.
+                                </p>
+
+                                <div className="tags flex flex-wrap items-center gap-4">
+                                    <span>JavaScript</span>
+                                    <span>TypeScript</span>
+                                    <span>React</span>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+
+                    <div id="projects" className="flex flex-col gap-8 -mx-4">
+                        <h2 className="text-sm font-bold text-emerald-600 -mb-6 px-4">PROJECTS</h2>
+
+                        <a href="#" className="grid grid-cols-[125px_auto] hover:bg-emerald-900/50 transition rounded-lg p-4">
+                            <span className="timeframe text-xs font-bold text-zinc-400 mt-1">2024 — PRESENT</span>
+                            <div className="details flex flex-col gap-2">
+                                <span className="pos-title text-lg font-bold">Senior Software Engineer</span>
+                                <p className="pos-description text-base text-zinc-300">
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae ultricies leo integer malesuada nunc vel risus commodo. Ac tincidunt vitae semper quis lectus nulla at volutpat diam.
+                                </p>
+
+                                <div className="tags flex flex-wrap items-center gap-4">
+                                    <span>JavaScript</span>
+                                    <span>TypeScript</span>
+                                    <span>React</span>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </main >
+    );
 }
